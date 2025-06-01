2d0b39d637ba0a38e5a69693361143a46d13aa29
R-Mohana-Priya <itzmemohana@gmail.com>
2025-06-01 21:48:33 +0530

    remove certain files

--- tests/test_display_products.py
+++
@@ -1,9 +0,0 @@
-# tests/test_display_products.py
-
-def test_products_displayed(page):
-    items = page.locator(".inventory_item")
-    assert items.count() > 0
-
-    for i in range(items.count()):
-        assert items.nth(i).locator(".inventory_item_name").is_visible()
-        assert items.nth(i).locator(".inventory_item_price").is_visible()
--- tests/test_logout.py
+++
@@ -1,6 +0,0 @@
-# tests/test_logout.py
-
-def test_logout(page):
-    page.click("#react-burger-menu-btn")
-    page.locator("#logout_sidebar_link").click()
-    assert page.url == "https://www.saucedemo.com/"
